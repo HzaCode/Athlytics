188a43ddf370e87a791f37a8c69937a741bcbf04
Ang <hexiaoang@gmail.com>
2025-05-02 15:03:46 +0800

    add zzz

---
+++ R/zzz.R
@@ -0,0 +1,19 @@
+# R/zzz.R
+
+.onAttach <- function(libname, pkgname) {
+  
+  pkg_desc <- utils::packageDescription(pkgname)
+  pkg_version <- pkg_desc$Version
+  
+
+  startup_msg <- paste0(
+    "\nLoading Athlytics version ", pkg_version, ".\n",
+    "Analyze your Strava data with ease!\n",
+    "Remember to authenticate using rStrava::strava_oauth() first.\n",
+    "For documentation, see: https://hzacode.github.io/Athlytics/"
+  )
+  
+
+  packageStartupMessage(startup_msg)
+}
+
